a03330b1a67285f461dfc358db1e1ce927b84f12
Lucas Bastos do Nascimento <bastos.dev@edu.unifor.br>
2026-03-09 20:11:14 -0300

    implementando os logs de acesso (pq eu tinha esquecido)

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,9 @@
 // backend/src/server.js
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan'); // Importa o morgan
 require('dotenv').config();
 
-// Import do Swagger
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
@@ -15,7 +15,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev')); // logs de acesso no terminal
 
+// Registro da documentacao do Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Registro das rotas principais
@@ -28,6 +30,6 @@ app.get('/health', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server rodando na porta ${PORT}`);
-  console.log(`Doc disponível em http://localhost:${PORT}/api-docs`);
+  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Documentação disponível em http://localhost:${PORT}/api-docs`);
 });
